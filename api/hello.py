f3551664bb964537d6f6433de6293011983a84d9
skvmdm <82228898+skvmdm@users.noreply.github.com>
2022-06-26 01:41:12 +0300

    Update hello.py

--- api/hello.py
+++ api/hello.py
@@ -1,4 +1,5 @@
-from http.server import BaseHTTPRequestHandler class handler(BaseHTTPRequestHandler):
+from http.server import BaseHTTPRequestHandler 
+class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     self.send_response(200)
     self.send_header('Content-type','text/plain')
