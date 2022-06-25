79f1b05056acb305eb9d8db5cd2169782c1e02b1
skvmdm <82228898+skvmdm@users.noreply.github.com>
2022-06-26 00:48:13 +0300

    Update skill.py

--- api/skill.py
+++ api/skill.py
@@ -1,6 +1,8 @@
 import aiohttp
 from aiohttp import web
 
+app = web.Application()
+
 HOST_IP = "0.0.0.0"
 HOST_PORT = 1254
 
@@ -19,7 +21,7 @@ async def skill_space(request_obj):
 
 
 def init():
-    app = web.Application()
+    #app = web.Application()
     app.router.add_post("/skill_space", skill_space)
     web.run_app(app, host = HOST_IP, port = HOST_PORT)
 
