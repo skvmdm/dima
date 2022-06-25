2dd7ce80c62ac646ec203f3cb66283f4fdb14588
skvmdm <82228898+skvmdm@users.noreply.github.com>
2022-06-25 22:45:56 +0300

    Create skill.py

---
+++ api/skill.py
@@ -0,0 +1,27 @@
+import aiohttp
+from aiohttp import web
+
+HOST_IP = "0.0.0.0"
+HOST_PORT = 1254
+
+async def skill_space(request_obj):
+    request = await request_obj.json()
+
+    response = {}
+    response["version"] = request["version"]
+    response["session"] = request["session"]
+    response["response"] = {"end_session" : False}
+sddsdf
+    response["response"]["text"] = "Привет!"
+    response["response"]["end_session"] = True
+
+    return web.json_response(response)
+
+
+def init():
+    app = web.Application()
+    app.router.add_post("/skill_space", skill_space)
+    web.run_app(app, host = HOST_IP, port = HOST_PORT)
+
+if __name__=="__main__":
+    init()
