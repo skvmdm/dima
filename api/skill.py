05754631b40af664997c2465601101fc265c5fb5
skvmdm <82228898+skvmdm@users.noreply.github.com>
2022-06-26 01:09:25 +0300

    Update skill.py

--- api/skill.py
+++ api/skill.py
@@ -1,8 +1,6 @@
 import aiohttp
 from aiohttp import web
 
-app = web.Application()
-
 HOST_IP = "0.0.0.0"
 HOST_PORT = 1254
 
@@ -20,10 +18,17 @@ async def skill_space(request_obj):
     return web.json_response(response)
 
 
-def init():
+#def init():
     #app = web.Application()
-    app.router.add_post("/skill_space", skill_space)
-    web.run_app(app, host = HOST_IP, port = HOST_PORT)
+    #app.router.add_post("/skill_space", skill_space)
+    #web.run_app(app, host = HOST_IP, port = HOST_PORT)
 
-if __name__=="__main__":
-    init()
+#if __name__=="__main__":
+    #init()
+
+app = web.Application()
+app.add_routes([web.get('/', skill_space),
+                web.get('/{name}', skill_space)])
+
+if __name__ == '__main__':
+    web.run_app(app)
