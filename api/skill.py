02f27ba530f4ca6b0a27c9994c983203085f15a9
skvmdm <82228898+skvmdm@users.noreply.github.com>
2022-06-26 02:04:55 +0300

    Update skill.py

--- api/skill.py
+++ api/skill.py
@@ -1,40 +1,26 @@
 import aiohttp
 from aiohttp import web
 
-async def skill_space(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
+HOST_IP = "0.0.0.0"
+HOST_PORT = 1254
 
-app = web.Application()
-app.router.add_post("/skill_space", skill_space)
+async def skill_space(request_obj):
+    request = await request_obj.json()
 
-if __name__ == '__main__':
-    web.run_app(app)
-    
+    response = {}
+    response["version"] = request["version"]
+    response["session"] = request["session"]
+    response["response"] = {"end_session" : False}
+    response["response"]["text"] = "Привет!"
+    response["response"]["end_session"] = True
 
-#HOST_IP = "0.0.0.0"
-#HOST_PORT = 1254
-
-#async def skill_space(request_obj):
-#    request = await request_obj.json()
-
-#    response = {}
-#    response["version"] = request["version"]
-#    response["session"] = request["session"]
-#    response["response"] = {"end_session" : False}
-
-#    response["response"]["text"] = "Привет!"
-#    response["response"]["end_session"] = True
-
-#    return web.json_response(response)
+    return web.json_response(response)
 
 
-#def init():
-    #app = web.Application()
-    #app.router.add_post("/skill_space", skill_space)
-    #web.run_app(app, host = HOST_IP, port = HOST_PORT)
+def init():
+    app = web.Application()
+    app.router.add_post("/skill_space", skill_space)
+    web.run_app(app, host = HOST_IP, port = HOST_PORT)
 
-#if __name__=="__main__":
-    #init()
-
+if __name__=="__main__":
+    init()
