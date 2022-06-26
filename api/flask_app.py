ee7e9f6f6918aadbeb4c076333f9517b7793aee7
skv <skv@otr.ru>
2022-06-26 13:04:21 +0300

    для pythonanywhere

---
+++ api/flask_app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route('/privet', methods = ['POST'])
+def update_text():
+    content = request.json
+    status = '200 OK'
+    #output = b'Hello World!\n'
+
+    response = {}
+    response["version"] = content["version"]
+    response["session"] = content["session"]
+    response["response"] = {"end_session" : False}
+    response["response"]["text"] = "Привет!"
+    response["response"]["end_session"] = True
+    return response
